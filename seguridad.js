6f50d6c2e93947c693697302cfd7313bd1eb8c76
migarcia0817 <migarcia0817@gmail.com>
2026-04-16 15:47:59 -0400

    Update access control logic for Supervisor role

--- seguridad.js
+++ seguridad.js
@@ -22,6 +22,7 @@
     // 4. VALIDACIÓN DE PERMISOS PARA EL SUPERVISOR
     if (tipoUsuario === "Supervisor") {
         const paginasPermitidas = [
+            "principal.html",
             "reportes.html",
             "reporteventas.html",
             "listajugada.html",
